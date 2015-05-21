bba4531d46c783d1b62a923d6c7b6ad831666388
Yohei Okubo <okubo.yohei@gmail.com>
2015-05-21 12:12:13 +0900

    cell

--- TableView/TableView/FirstViewController.swift
+++ TableView/TableView/FirstViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
+    let items = [
+        "item 1",
+        "item 2",
+        "item 3",
+        "item 4",
+        "item 5",
+        "item 6",
+        "item 7",
+        "item 8",
+        "item 9",
+        "item 10",
+        "item 11",
+        "item 12"]
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "SimpleTable"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //テーブルの行数を返す
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
     }
 
+
+    //各セルに表示する内容を決定するindexPathで指定された場所のセルを作る
+    //UITableViewDataSourceProtocol
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let identifier = "default-cell"
+        //セルの再利用
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as! UITableViewCell
+
+    }
 
 }
 

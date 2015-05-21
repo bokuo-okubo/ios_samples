cf42e4bb1cbcc0160fe851638879cf2da65d4136
Yohei Okubo <okubo.yohei@gmail.com>
2015-05-21 13:19:11 +0900

    table view

--- TableView/TableView/FirstViewController.swift
+++ TableView/TableView/FirstViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class FirstViewController: UITableViewController {
+class FirstViewController:
+UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     let items = [
         "item 1",
         "item 2",
@@ -23,30 +25,60 @@ class FirstViewController: UITableViewController {
         "item 11",
         "item 12"]
 
+    private var myTableView : UITableView!
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        //get status bar height
+        let barHeight = UIApplication.sharedApplication().statusBarFrame.size.height
+
+        //get windowsize
+        let displayWidth = self.view.frame.width
+        let displayHeight = self.view.frame.height
+
+        myTableView = UITableView(frame:
+            CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight),
+            style: UITableViewStyle.Plain)
+
+        myTableView.registerClass( UITableViewCell.self ,
+            forCellReuseIdentifier: "default-cell")
+
+        myTableView.dataSource = self
+        myTableView.delegate = self
+
         self.title = "SimpleTable"
+
+        self.view.addSubview(myTableView)
+    }
+
+    func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        println("NUM: \(indexPath.row)")
+        println("value: \(items[indexPath.row])")
     }
 
     //テーブルの行数を返す
-    override func tableView(tableView: UITableView,
+    func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int
     {
         return items.count
     }
 
-
     //各セルに表示する内容を決定するindexPathで指定された場所のセルを作る
     //UITableViewDataSourceProtocol
-    override func tableView(tableView: UITableView,
+    func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let identifier = "default-cell"
         //セルの再利用
-        let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier,
+            forIndexPath: indexPath) as! UITableViewCell
+        //Question: Objective-Cだとcellのnilチェックをしてなければ生成するけど、しなくていいのはなぜ？
+        cell.textLabel?.text = items[indexPath.row]
 
+        return cell
     }
-
 }
 
